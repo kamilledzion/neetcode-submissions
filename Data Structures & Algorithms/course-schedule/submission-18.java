87c63191e2b5828bac4db3e04f5a0c7a8c82a866
Kamil <kamil.ledzion@gmail.com>
2026-08-19 14:08:14 +0200

    Add: course-schedule - submission-18

---
+++ Data Structures & Algorithms/course-schedule/submission-18.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int from = prerequisites[i][0];
+            int to = prerequisites[i][1];
+
+            map.computeIfAbsent(from, v -> new ArrayList<>()).add(to);
+            indegree[to]++;
+        }
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.addLast(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            Integer node = queue.pollFirst();            
+            visited++;
+
+            if (map.get(node) != null) {
+                for (Integer nei : map.get(node)) {
+                    indegree[nei]--;
+                    if (indegree[nei] == 0) {
+                        queue.addLast(nei);
+                    }
+                }
+            }
+        }
+
+        return visited == numCourses;
+    }
+}
+
