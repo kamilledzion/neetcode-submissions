4cfec73f7a9e32c02b5d363a0b222ae5f1feb2ef
Kamil <kamil.ledzion@gmail.com>
2026-08-20 23:02:47 +0200

    Add: count-paths - submission-6

---
+++ Data Structures & Algorithms/count-paths/submission-6.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    Map<String, Integer> mem = new HashMap<>();
+
+    public int uniquePaths(int m, int n) {
+        return dfs(0, 0, m, n);
+    }
+
+    int dfs(int row, int col, int maxRow, int maxCol) {
+        if (row == maxRow || col == maxCol) {
+            return 0;
+        }
+        if (row == maxRow - 1 && col == maxCol - 1) {
+            return 1;
+        }
+        var key = row + "," + col;
+        if (mem.containsKey(key)) {
+            return mem.get(key);
+        }
+
+        var res = dfs(row + 1, col, maxRow, maxCol) + dfs(row, col + 1, maxRow, maxCol);
+        mem.put(key, res);
+
+        return res;
+    }
+}
